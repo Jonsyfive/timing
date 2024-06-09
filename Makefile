2640870c4cb4151a2a384a4bd69549d5dcc6b721
Jonathan Schmoll <jonathan.schmoll@cloudpilots.com>
2024-06-09 19:30:36 +0200

    Add other functions, add ftz enable for raspberry

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 fastestrng:  benchmark.cpp
 	c++ --version
-	c++ -std=c++17 -O2 -fno-tree-vectorize -o benchmark benchmark.cpp  -I include -Wall -Wextra
+	c++ -std=c++17 -O0 -fno-tree-vectorize -o benchmark benchmark.cpp  -I include -Wall -Wextra
 	echo "run sudo ./benchmark"
 clean:
 	rm -r -f benchmark
--- benchmark.cpp
+++ benchmark.cpp
@@ -17,35 +17,7 @@
 #include <random>
 #include <sstream>
 #include <stdio.h>
-// #include <fenv.h>
 #include <vector>
-
-double multiply_doubles(size_t repeat, double double_1, double double_2)
-{
-
-  double res;
-
-  for (size_t i = 0; i < repeat; i++)
-  {
-
-    res = double_1 * double_2;
-  }
-
-  return res;
-}
-
-double divide_doubles(size_t repeat, double double_1, double double_2)
-{
-  double res;
-
-  for (size_t i = 0; i < repeat; i++)
-  {
-
-    res = double_1 / double_2;
-  }
-
-  return res;
-}
 
 float multiply_floats(size_t repeat, float float_1, float float_2)
 {
@@ -91,12 +63,11 @@ float sub_floats(size_t repeat, float float_1, float float_2)
 {
   (void)repeat;
   float res;
+  for (size_t i = 0; i < repeat; i++)
+  {
 
-  // for (size_t i = 0; i < repeat; i++) {
-
-  res = float_1 - float_2;
-
-  // }
+    res = float_1 - float_2;
+  }
 
   return res;
 }
@@ -117,7 +88,6 @@ event_aggregate time_it_ns(T const &function, size_t repeat, float float_1, floa
       printf("bug\n");
     }
   }
-  // for (size_t i = 0; i < repeat; i++) {
   collector.start();
   float ts = function(repeat, float_1, float_2);
   if (ts < 0)
@@ -126,7 +96,6 @@ event_aggregate time_it_ns(T const &function, size_t repeat, float float_1, floa
   }
   event_count allocate_count = collector.end();
   aggregate << allocate_count;
-  // }
   return aggregate;
 }
 
@@ -163,41 +132,45 @@ void write_to_output(size_t repeat, event_aggregate aggregate, std::string name,
 {
   std::ofstream output;
   output.open(name + ".txt", std::ios_base::app);
+
   output << name << " " << aggregate.cycles() / repeat << " " << aggregate.best.instructions() / aggregate.best.cycles() << " " << float_1 << " " << float_2 << "\n";
   output.close();
 }
 
-void process_floats(size_t repeat, float float_1, float float_2, std::string name)
+void process_divide_floats(size_t repeat, float float_1, float float_2, std::string name)
 {
-  // pretty_print(repeat, "Divide",
-  //              time_it_ns( divide_floats, repeat));
-  //  pretty_print(repeat, "Multiply",
-  //  time_it_ns( multiply_doubles, repeat));
+
   write_to_output(repeat, time_it_ns(divide_floats, repeat, float_1, float_2), name, float_1, float_2);
   printf("\n");
 }
 
-// void process_doubles( size_t repeat, double double_1, double double_2) {
-//   pretty_print(repeat, "Divide",
-//                time_it_ns( divide_doubles, repeat));
-//   write_to_output(repeat,time_it_ns( divide_doubles, repeat), "Divide");
+void process_multiply_floats(size_t repeat, float float_1, float float_2, std::string name)
+{
 
-// }
+  write_to_output(repeat, time_it_ns(multiply_floats, repeat, float_1, float_2), name, float_1, float_2);
+  printf("\n");
+}
+
+void process_add_floats(size_t repeat, float float_1, float float_2, std::string name)
+{
+
+  write_to_output(repeat, time_it_ns(add_floats, repeat, float_1, float_2), name, float_1, float_2);
+  printf("\n");
+}
+
+void process_sub_floats(size_t repeat, float float_1, float float_2, std::string name)
+{
+
+  write_to_output(repeat, time_it_ns(sub_floats, repeat, float_1, float_2), name, float_1, float_2);
+  printf("\n");
+}
 
 int main()
 {
   float args[10] = {0.0, 1.0, 255, 256, 257, 1.0e-42, 1.0e-41, 1.0e-30, 1.0e30, 1.0e10};
   float float_1;
   float float_2;
-  size_t repeat = 1000000000;
-  // int r = fesetenv(FE_DFL_ENV);
-
-  // if (r != 0)
-  // {
-  //   fprintf(stderr, "fesetenv returned %d\n", r);
-  //   return -1;
-  // }
-  // printf("fesetenv returned %d\n", r);
+  size_t repeat = 1000000;
 
   for (size_t i = 0; i < 10; i++)
   {
@@ -205,29 +178,88 @@ int main()
     for (size_t j = 0; j < 10; j++)
     {
       float_2 = args[j];
-      process_floats(repeat, float_1, float_2, "Divide");
-      process_floats(repeat, float_2, float_1, "Divide");
+      process_divide_floats(repeat, float_1, float_2, "Divide");
+      process_divide_floats(repeat, float_2, float_1, "Divide");
     }
   }
 
-  // r = fesetenv(FE_DFL_DISABLE_DENORMS_ENV);
+  for (size_t i = 0; i < 10; i++)
+  {
+    float_1 = args[i];
+    for (size_t j = 0; j < 10; j++)
+    {
+      float_2 = args[j];
+      process_multiply_floats(repeat, float_1, float_2, "Mulitply");
+      process_multiply_floats(repeat, float_2, float_1, "Multiply");
+    }
+  }
 
-  // if (r != 0)
-  // {
-  //   fprintf(stderr, "fesetenv returned %d\n", r);
-  //   return -1;
-  // }
-  // printf("fesetenv returned %d\n", r);
-  // for (size_t i = 0; i < 10; i++)
-  // {
-  //   float_1 = args[i];
-  //   for (size_t j = 0; j < 10; j++)
-  //   {
-  //     float_2 = args[j];
-  //     process_floats(repeat, float_1, float_2, "DivideFTZ");
-  //     process_floats(repeat, float_2, float_1, "DivideFTZ");
-  //   }
-  // }
+  for (size_t i = 0; i < 10; i++)
+  {
+    float_1 = args[i];
+    for (size_t j = 0; j < 10; j++)
+    {
+      float_2 = args[j];
+      process_add_floats(repeat, float_1, float_2, "Add");
+      process_add_floats(repeat, float_2, float_1, "Add");
+    }
+  }
+
+  for (size_t i = 0; i < 10; i++)
+  {
+    float_1 = args[i];
+    for (size_t j = 0; j < 10; j++)
+    {
+      float_2 = args[j];
+      process_sub_floats(repeat, float_1, float_2, "Sub");
+      process_sub_floats(repeat, float_2, float_1, "Sub");
+    }
+  }
+
+  asm("vmsr fpscr,%0" ::"r"(1 << 24));
+  for (size_t i = 0; i < 10; i++)
+  {
+    float_1 = args[i];
+    for (size_t j = 0; j < 10; j++)
+    {
+      float_2 = args[j];
+      process_divide_floats(repeat, float_1, float_2, "DivideFTZ");
+      process_divide_floats(repeat, float_2, float_1, "DivideFTZ");
+    }
+  }
+
+  for (size_t i = 0; i < 10; i++)
+  {
+    float_1 = args[i];
+    for (size_t j = 0; j < 10; j++)
+    {
+      float_2 = args[j];
+      process_multiply_floats(repeat, float_1, float_2, "MulitplyFTZ");
+      process_multiply_floats(repeat, float_2, float_1, "MultiplyFTZ");
+    }
+  }
+
+  for (size_t i = 0; i < 10; i++)
+  {
+    float_1 = args[i];
+    for (size_t j = 0; j < 10; j++)
+    {
+      float_2 = args[j];
+      process_add_floats(repeat, float_1, float_2, "AddFTZ");
+      process_add_floats(repeat, float_2, float_1, "AddFTZ");
+    }
+  }
+
+  for (size_t i = 0; i < 10; i++)
+  {
+    float_1 = args[i];
+    for (size_t j = 0; j < 10; j++)
+    {
+      float_2 = args[j];
+      process_sub_floats(repeat, float_1, float_2, "SubFTZ");
+      process_sub_floats(repeat, float_2, float_1, "SubFTZ");
+    }
+  }
 
   if (!collector.has_events())
   {
